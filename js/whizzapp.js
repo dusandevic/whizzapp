521016770764e8881f0f25c7654079d6248c663b
dusandevic@gmail.com <dusandevic@gmail.com>
2013-09-18 02:34:16 +0200

    new pages

---
+++ js/whizzapp.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$(document).bind("mobileinit", function(){
+  $.extend(  $.mobile , {
+    defaultPageTransition: 'none',
+  });    
+  	$.mobile.page.prototype.options.addBackBtn = true;
+});
+
