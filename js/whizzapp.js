9d700947a183e9363ed0e03f559ca9f09b650a28
dusandevic@gmail.com <dusandevic@gmail.com>
2013-09-19 01:14:01 +0200

    new look

--- js/whizzapp.js
+++ js/whizzapp.js
@@ -1,9 +1,8 @@
 // JavaScript Document
 $(document).bind("mobileinit", function(){
   $.extend(  $.mobile , {
-    defaultPageTransition: 'fade',
+    defaultPageTransition: 'slide',
   });    
   	$.mobile.page.prototype.options.addBackBtn = true;
-	$.mobile.buttonMarkup.hoverDelay = 50;
 });
 
