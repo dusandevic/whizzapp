4391aa9840f1936fab298e61ff709b5f5e333caa
dusandevic@gmail.com <dusandevic@gmail.com>
2013-09-20 02:43:29 +0200

    swipe milestones

--- js/whizzapp.js
+++ js/whizzapp.js
@@ -4,5 +4,25 @@ $(document).bind("mobileinit", function(){
     defaultPageTransition: 'slide',
   });    
   	$.mobile.page.prototype.options.addBackBtn = true;
+	
+	$(function(){
+	  var mySwiper = $('.swiper-container').swiper({
+		//Your options here:
+		mode:'horizontal',
+		loop: true,
+		calculateHeight: true,
+		slidesPerView: 3,
+		freeMode: false,
+		resizeReInit: true,
+		
+		tdFlow: {
+			rotate : 0,
+			stretch :0,
+			depth: 150,
+			modifier : 1,
+			shadows : true
+    	}	
+		});
+	})
 });
 
