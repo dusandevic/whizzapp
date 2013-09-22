015826270508669bb5800d9e1f9fe00c93bc12b9
dusandevic@gmail.com <dusandevic@gmail.com>
2013-09-22 05:57:14 +0200

    iTunes milestones fix

--- js/whizzapp.js
+++ js/whizzapp.js
@@ -1,28 +1,21 @@
 // JavaScript Document
 $(document).bind("mobileinit", function(){
   $.extend(  $.mobile , {
-    defaultPageTransition: 'slide',
+    defaultPageTransition: 'none',
   });    
   	$.mobile.page.prototype.options.addBackBtn = true;
 	
-	$(function(){
+	$(window).resize();
+
+	$(function(){	  
 	  var mySwiper = $('.swiper-container').swiper({
-		//Your options here:
-		mode:'horizontal',
-		loop: true,
-		calculateHeight: true,
-		slidesPerView: 3,
-		freeMode: false,
-		resizeReInit: true,
-		
-		tdFlow: {
-			rotate : 0,
-			stretch :0,
-			depth: 150,
-			modifier : 1,
-			shadows : true
-    	}	
+			slidesPerView:2,
+			centeredSlides: true,
+			loop:true,
+			tdFlow: {}	
 		});
-	})
+		
+ 	
+	})	
 });
 
